b555e6a1838ba13237176f33d1af106bbf216253
Zsj <jshazsj@qq.com>
2018-11-17 02:28:32 +0800

    Initial Commit

---
+++ src/main/java/com/online/stuauth/StuauthApplication.java
@@ -0,0 +1,12 @@
+package com.online.stuauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StuauthApplication.class, args);
+    }
+}
